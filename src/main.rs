1c30a16c2ca060f47c974d2db9934c4e6d8ce1bc
codingMaz <m.balandari.toroghi@gmail.com>
2024-01-03 14:38:26 +0330

    final touch!

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(0..=100);
-    println!("The secret number is {secret_number}");
     
     loop {
         println!("Please input your guess!");
